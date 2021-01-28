aa5dea79c0b777b660ba35e56ea7f85aabb3aef1
jkeown <jd.keown@yahoo.com>
2021-01-28 10:18:33 -0500

    footer text color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,9 @@ function getCurrentYear() {
 function Footer() {
   return (
     <footer className="flex flex-col items-center p-8 pt-10 bg-black text-white mt-12">
-      <div className=" text-red-500 font-bold text-center uppercase tracking-wide">
+      <div className="font-bold text-center uppercase tracking-wide">
         <p>Don't miss out on the latest styles!</p>
-        <p>Follow us on Social media.</p>
+        <p className="text-red-500">Follow us on Social media.</p>
       </div>
       <div className="icons flex justify-between w-1/2 md:w-1/4 my-8">
         <a href="#"><img src={instagram} alt="instagram icon" /></a>
