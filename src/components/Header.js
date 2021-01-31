6997958f9b6e8da3380b875a698f82877ffc3825
jkeown <jd.keown@yahoo.com>
2021-01-30 21:10:34 -0500

    set header height

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Nav from "./Nav"
 
 function Header() {
   return (
-    <header className="flex justify-center px-4 py-2 bg-black text-white fixed w-full z-10">
+    <header className="h-28 flex justify-center px-4 py-2 bg-black text-white fixed w-full z-10">
       <div className="header-container flex justify-between items-center w-full md:w-8/12 lg:w-6/12">
         <Logo />
         <Nav />
