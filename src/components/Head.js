0f02f703ae9f685256bcf5240b4b276f988910f7
jkeown <jd.keown@yahoo.com>
2021-02-03 17:20:25 -0500

    social media image meta tag png

--- src/components/Head.js
+++ src/components/Head.js
@@ -25,7 +25,7 @@ const Head = () => {
                 content="The finest hats and purses"
             />
             {/* STILL NEED TO UPDATE IMAGE BELOW */}
-            <meta property="og:image" content="https://natinatash.com/logo-sm.svg" />
+            <meta property="og:image" content="https://natinatash.com/logo-sm.png" />
 
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
@@ -37,7 +37,7 @@ const Head = () => {
             />
             {/* STILL NEED TO UPDATE IMAGE BELOW */}
 
-            <meta property="twitter:image" content="https://natinatash.com/logo-sm.svg" />
+            <meta property="twitter:image" content="https://natinatash.com/logo-sm.png" />
         </Helmet>
     )
 }
